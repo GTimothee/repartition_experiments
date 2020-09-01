a6bfe51ff61d413c5ef6f0926c8a7857e08ac0d5
GTimothee <ava_punksmash@protonmail.com>
2020-09-01 15:08:49 +0200

    added y_limit to statistics

--- repartition_experiments/statistics.py
+++ repartition_experiments/statistics.py
@@ -147,6 +147,7 @@ def compute_graph_results(results_path, outdir_path, title_main, title_seeks):
     ax.set_title(title_main)
     ax.set_xticks(x)
     ax.set_xticklabels(sorted(references))
+    ax.set_ylim(top=1600)
     ax.legend()
 
     fig.tight_layout()
