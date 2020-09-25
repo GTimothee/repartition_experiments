ad2ceca3dc4b70ce411c4b49e93039e37b4902cb
GTimothee <ava_punksmash@protonmail.com>
2020-09-25 14:23:46 +0200

    first version of simulator

---
+++ repartition_experiments/scripts_paper/baseline_simulator.py
@@ -0,0 +1,58 @@
+import numpy as np
+
+from repartition_experiments.algorithms.utils import get_blocks_shape, get_named_volumes, hypercubes_overlap, get_overlap_subarray, Volume
+
+
+def get_overlap_volume(v1, v2):
+    pair = get_overlap_subarray(v1, v2)  # overlap coordinates in basis of R
+    p1, p2 = tuple(pair[0]), tuple(pair[1])
+    return Volume(0, p1, p2)
+
+
+def write_buffer(involume, outvolume, O):
+    overlap_shape = get_overlap_volume(involume, outvolume).get_shape()
+
+    nb_outfile_seeks_tmp = 0
+    s = overlap_shape
+    if s[2] != O[2]:
+        nb_outfile_seeks_tmp += s[0]*s[1]
+    elif s[1] != O[1]:
+        nb_outfile_seeks_tmp += s[0]
+    elif s[0] != O[0]:
+        nb_outfile_seeks_tmp += 1
+    else:
+        pass
+
+    return nb_outfile_seeks_tmp
+
+
+def baseline_rechunk(O, I, R):
+    """
+    Arguments: 
+    ----------
+        O, I, R: tuples
+    """
+
+    infiles_partition = get_blocks_shape(R, I)
+    inblocks = get_named_volumes(infiles_partition, I)
+    
+    outfiles_partition = get_blocks_shape(R, O)
+    outblocks = get_named_volumes(outfiles_partition, O)
+
+    nb_infile_openings = 0
+    nb_infile_seeks = 0
+    nb_outfile_openings = 0
+    nb_outfile_seeks = 0
+
+    for buffer_index in sorted(inblocks.keys()):
+        print(f"Nb elements: {len(inblocks.keys())}, index: {buffer_index}")
+        read_buffer = inblocks[buffer_index]
+        nb_infile_openings += 1
+
+        for outblock in outblocks.values():
+            if hypercubes_overlap(read_buffer, outblock):
+                nb_outfile_seeks_tmp = write_buffer(read_buffer, outblock, O)
+                nb_outfile_seeks += nb_outfile_seeks_tmp
+                nb_outfile_openings += 1
+
+    return nb_outfile_openings + nb_outfile_seeks + nb_infile_openings + nb_infile_seeks
---
+++ repartition_experiments/scripts_paper/seek_model_verification.py
@@ -0,0 +1,132 @@
+import random, json, argparse, os, sys
+
+
+def get_random_array_shape():
+    """ Generate a random original array shape.
+    """
+    PRIME_NUMBERS = [2, 3, 5, 7, 11]
+    POWERS = [1, 2, 3]
+
+    length = 0
+    for _ in range(3):
+        number = PRIME_NUMBERS[random.randint(0,4)]
+        power = POWERS[random.randint(0,2)]
+        if length == 0:
+            length = number**power
+        else:
+            length *= number**power
+
+    return (length, length, length)
+
+
+def get_random_cases(limit, model):
+    """ Get random cases (A,O,I,B) to test
+    """
+    A = get_random_array_shape()
+    divisors = get_divisors(A[0])
+    random.shuffle(divisors)
+    cases_list = list()
+    print(f"Found A: {A}")
+    print(f"Divisors: {divisors}")
+
+    i = 0
+    while i < len(divisors):
+
+        I = (divisors[i], divisors[i], divisors[i])
+        O = (divisors[i+1], divisors[i+1], divisors[i+1])
+        print(f"I: {I}")
+        print(f"O: {O}")
+
+        if model == "keep":
+            candidates = get_buffer_candidates({"R": A, "I": I, "O": O})
+            print(f"Number candidates found: {len(candidates)}")
+            _range = min(5, len(candidates))
+            for i in range(_range):
+                cases_list.append((A, I, O, candidates[random.randint(0,len(candidates)-1)]))
+              
+            print(f"Buffer candidates selected: {candidates}")
+        else:
+            cases_list.append((A, I, O, I))
+
+        # inverse I and O 
+        O = (divisors[i], divisors[i], divisors[i])
+        I = (divisors[i+1], divisors[i+1], divisors[i+1])
+        print(f"I: {I}")
+        print(f"O: {O}")
+
+        if model == "keep":
+            candidates = get_buffer_candidates({"R": A, "I": I, "O": O})
+            print(f"Number candidates found: {len(candidates)}")
+            _range = min(5, len(candidates))
+            for i in range(_range):
+                cases_list.append((A, I, O, candidates[random.randint(0,len(candidates)-1)]))
+            print(f"Buffer candidates selected: {candidates}")
+        else:
+            cases_list.append((A, I, O, I))
+
+        if len(cases_list) == limit:
+            return cases_list
+        elif len(cases_list) > limit:
+            return cases_list[:limit]
+        else:
+            i += 2
+
+    return cases_list
+
+
+def get_arguments():
+    """ Get arguments from console command.
+    """
+    parser = argparse.ArgumentParser(description="")
+    
+    parser.add_argument('paths_config', 
+        action='store', 
+        type=str, 
+        help='Path to configuration file containing paths of data directories.')
+
+    return parser.parse_args()
+
+
+def load_json(filepath):
+    with open(filepath) as f:
+        return json.load(f)
+
+
+if __name__ == "__main__":
+
+    args = get_arguments()
+    paths = load_json(args.paths_config)
+    for k, v in paths.items():
+        if "PYTHONPATH" in k:
+            sys.path.insert(0, v)
+
+    from repartition_experiments.scripts_paper.baseline_seeks_model_remake import compute_nb_seeks_model
+    from repartition_experiments.scripts_exp.seek_calculator import get_buffer_candidates, get_divisors
+    from repartition_experiments.scripts_paper.baseline_simulator import baseline_rechunk
+
+    # parameters
+    seed = 42
+    number_tests = 1000
+    nb_case_per_A = 10
+    model = "baseline"
+
+    nb_tests = 0
+    random.seed(seed)
+    while nb_tests < number_tests:
+        print(f"Computing new cases....")
+        cases_list = get_random_cases(nb_case_per_A, model)
+        print(f"End.")
+
+        for case in cases_list:
+            A, I, O, B = case
+            predicted = compute_nb_seeks_model(A, I, O)
+            reality = baseline_rechunk(O, I, A)
+
+            print(f"Case: {case}")
+            print(f"Predicted: {predicted}")
+            print(f"Reality: {reality}")
+
+            if predicted != reality:
+                raise ValueError("Error")
+
+        nb_tests += len(cases_list)
